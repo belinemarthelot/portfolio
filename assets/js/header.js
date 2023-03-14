a450b5cc1bdc868e7b818b09a5bb50a2a4e77c58
MaxenceLiboz <maxoulib@gmail.com>
2023-03-14 19:36:50 +0100

    Feat (header): Header data is in one file and menu is working

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,65 +1,62 @@
-function insertHeader() {
-	var headerContainer = document.getElementById('script-header-container');
-	// var baseUrl = window.location.origin;
-    var baseUrl = "https://belinemarthelot.github.io/portfolio"
-	headerContainer.innerHTML = `
-    <!-- Header -->
-    <header id="header">
-        <h1><a href="${baseUrl}/index.html">Page d'accueil</a></h1>
-        <nav class="links">
-            <ul>
-                <li><a href="${baseUrl}/pages/prepa.html">Année de prépa</a></li>
-                <li>
-                    <a href="${baseUrl}/pages/first_year_bts.html">1er année de BTS</a>
-                </li>
-                <li>
-                    <a href="${baseUrl}/pages/second_year_bts.html">2ème année de BTS</a>
-                </li>
-                <li>
-                    <a href="${baseUrl}/pages/exp_pro.html">Éxperiences profesionnels</a>
-                </li>
-            </ul>
-        </nav>
-        <nav class="main">
-            <ul>
-                <li class="menu">
-                    <a class="fa-bars" href="#menu">Menu</a>
-                </li>
-            </ul>
-        </nav>
-    </header>
-    <!-- Menu -->
-    <section id="menu">
-        <!-- Links -->
-        <section>
-            <ul class="links">
-                <li>
-                    <a href="${baseUrl}/pages/prepa.html">
-                        <h3>Année de prépa</h3>
-                        <p>Préparation au CAP ésthetique</p>
+var baseUrl = window.location.origin;
+// var baseUrl = 'https://belinemarthelot.github.io/portfolio';
+function insertNavbars() {
+	// Fetch the JSON data
+	fetch('/assets/data/nav.json')
+		.then((response) => response.json())
+		.then((data) => {
+			// Get the navbar element
+			const elementsMainNavbar = document.getElementById(
+				'elements-main-navbar'
+			);
+			const headerMainNavbar =
+				document.getElementById('header-main-navbar');
+			const elementsMenuNavbar = document.getElementById(
+				'elements-menu-navbar'
+			);
+
+			// Create the navbar links
+			const elementsMainNavbarlinks = data.links
+				.slice(1)
+				.map(
+					(link) => `
+                        <li>
+                            <a href="${baseUrl}${link.url}">${link.title}</a>
+                        </li>
+                    `
+				)
+				.join('');
+
+			const headerMainNavbarlink = `
+                <a href="${baseUrl}${data.links[0].url}">${data.links[0].title}</a>
+            `;
+
+			// Create the navbar links
+			const elementsMenuNavbarlinks = data.links
+				.slice(1)
+				.map(
+					(link) => `
+                    <li>
+                    <a href="${baseUrl}${link.url}">
+                        <h3>${link.title}</h3>
+                        <p>${link.description}</p>
                     </a>
-                </li>
-                <li>
-                    <a href="${baseUrl}/pages/first_year_bts.html">
-                        <h3>1er année de BTS</h3>
-                        <p>Préparation au BTS MECP</p>
-                    </a>
-                </li>
-                <li>
-                    <a href="${baseUrl}/pages/second_year_bts.html">
-                        <h3>2ème année de BTS</h3>
-                        <p>Préparation au BTS MECP</p>
-                    </a>
-                </li>
-                <li>
-                    <a href="${baseUrl}/pages/exp_pro.html">
-                        <h3>Éxperiences profesionnels</h3>
-                        <p>Différents contrats CDD / extra</p>
-                    </a>
-                </li>
-            </ul>
-        </section>
-    </section>    
-    `;
+                    </li>
+                `
+				)
+				.join('');
+
+			// Insert the navbar HTML into the navbar element
+			headerMainNavbar.innerHTML = `
+               <ul>${headerMainNavbarlink}</ul>
+              `;
+			elementsMainNavbar.innerHTML = `
+            <ul>${elementsMainNavbarlinks}</ul>
+           `;
+           elementsMenuNavbar.innerHTML = `
+           <ul class="links">${elementsMenuNavbarlinks}</ul>
+          `;
+		});
 }
-window.onload = insertHeader;
+
+window.onload = insertNavbars;
